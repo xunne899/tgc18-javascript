6d9dd978e62406f4ff448612736f04c8b7590e32
kunxin-chor <chorkunxin@yahoo.com>
2022-05-20 07:19:56 +0000

    Added sedded sequences example

--- 05-sequences/string-basic.js
+++ 05-sequences/string-basic.js
@@ -1,6 +1,50 @@
+const prompt = require('prompt-sync')();
 let s = 'the quick brown fox';
 let n = "she sell seashells";
 console.log('she said,"I sold seashells"');
 console.log("I don't know");
 // all strings must be on a single line, cannot have newlines
-console.log("Dear Mum,\nI hope this letter finds you well");
+console.log("Dear Mum,\n\tI hope this letter finds you well");
+
+// escape sequences are for special characters that
+// confuses JavaScript if they are in a string
+console.log('She said,"I don\'t know what happenend!"');
+console.log("She said, \"she sells seashells at the seashore\"");
+console.log("C:\\My Documents\\test\\new_file.txt");
+
+// text literal or otherwise the backtick string
+console.log(`
+
+She said, "I sold seashells at the seashore"
+See ma. lot of enter
+My file is at C:/My Documents/test/new
+
+`);
+
+let firstName = "Ah Kow";
+let lastName = "Tan";
+let bill = 200;
+let formLetter =`Dear ${firstName} ${lastName},
+   Your bill of $${bill} is due soon. Please pay up soon or
+be fine 10% of your bill, which is $${0.1 * bill}.
+`
+console.log(formLetter);
+
+let name = prompt(`Name: `)
+
+console.log(`
+
+Dear ${name}:
+
+  I can't believe that it's been more than six months since we spoke. You remember the song?
+
+Should auld acquaintance be for forgot,
+and never be brought to mind?
+should auld acquaintance be forgot,
+and auld lang syne?
+
+"Merry X'mas and a Happy New Year!" 
+`);
+
+console.log("Dear " + name + ",\n\nI can't believe it has been more than six months since we we spoke." + 
+"You remember the song?\n");
---
+++ 05-sequences/string-slicing.js
@@ -0,0 +1,33 @@
+let s = "jack and jill went up the hill";
+console.log(s[7]); // => s[7] will simplify whatever is at index 7
+// for (let char of s) {
+//     console.log(char);
+// }
+// for (let i = 0; i < s.length; i++){
+//     console.log(i, s[i]);
+// }
+
+// IMMUTABLE => cannot be changed
+// strings are immutable and their content cannot be changed
+s[0] = 'Z'; // => s won't be changed 'cos strings are immutable
+console.log(s);
+
+// SLICING - allows us to get a range of characters from a string
+
+let message = "HELLO WORLD";
+// slice will take in one or two arguments
+console.log("message.slice(2,5) =>", message.slice(2, 5)); 
+console.log("message.slice(4,6) =>", message.slice(4,6));
+console.log('message after slicing =>', message);
+message = message.toLowerCase();
+// => message = "HELLO WORLD".toLowerCase();
+// => message = "hello world"
+console.log(message);
+
+// SLICE STRINGS WITH ONE ARGUMENT
+// assume start from that index and all the way to the end
+console.log("message.slice(7)=>",message.slice(7)); // same as message.slice(7, end of string)
+
+console.log('message.length =>', message.length);
+
+console.log(message.slice(-3));
